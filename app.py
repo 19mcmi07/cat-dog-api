442b06d42f343f4a9928806167dabd7b2d9e5088
Rohan Yadav <19mcmi07@gamil.com>
2020-05-30 12:37:00 +0530

    change message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'My pytorch model got deleted, so deployment will be delayed.'
 
 if __name__ == '__main__':
     app.run()
